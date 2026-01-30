6758a524af02ad9bc32dd02e99cc162ff82d8e98
Miloshevskaa <milamiloshevskaa@gmail.com>
2026-01-30 12:53:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const cover = document.getElementById("cover");
+
+cover.addEventListener("click", () => {
+    cover.style.opacity = "0";
+    setTimeout(() => {
+        cover.style.display = "none";
+    }, 500);
+});
+
+// ===== BUTTONS & MESSAGE =====
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const message = document.getElementById("message");
-const gif = document.getElementById("gif");
 
 let noClicks = 0;
 let yesScale = 1;
@@ -12,7 +21,6 @@ noBtn.addEventListener("click", () => {
 
     if (noClicks === 1) {
         noBtn.textContent = "Are you sure? 😢";
-        gif.src = "images/sad.gif";
     } 
     else if (noClicks === 2) {
         noBtn.textContent = "Please? 🥺";
@@ -37,21 +45,21 @@ noBtn.addEventListener("click", () => {
     } 
     else if (noClicks === 5) {
         noBtn.textContent = "Last chance 😭";
-        gif.src = "images/sad.gif";
     } 
     else {
         moveNoButton();
     }
 });
 
+// ===== NO BUTTON RUNS AWAY =====
 function moveNoButton() {
     const x = Math.random() * 300 - 150;
     const y = Math.random() * 300 - 150;
     noBtn.style.transform = `translate(${x}px, ${y}px) scale(${noScale})`;
 }
 
+// ===== YES BUTTON =====
 yesBtn.addEventListener("click", () => {
-    gif.src = "images/happy.gif";
     message.textContent = "YAYYYY!! 💖💖💖 I knew it!";
     yesBtn.style.display = "none";
     noBtn.style.display = "none";
